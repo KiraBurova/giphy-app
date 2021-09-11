94e5f501adb07e91b96a436838604e7cf0f007b0
Kira Burova <burovakira11@gmail.com>
2021-09-11 15:22:20 +0300

    fixed formatting

--- src/features/searchGifs/saga.ts
+++ src/features/searchGifs/saga.ts
@@ -1,12 +1,8 @@
 import { AxiosResponse } from 'axios';
-
 import { PayloadAction } from '@reduxjs/toolkit';
 import { takeEvery, all, put, SimpleEffect } from 'redux-saga/effects';
-
 import { MultiResponse } from 'giphy-api';
-
 import { searchGifsAPI } from '@/shared/api';
-
 import { actions } from './slice';
 
 function* searchGifs(action: PayloadAction<string>): Generator<Promise<AxiosResponse> | SimpleEffect<'PUT'>, void, { data: MultiResponse }> {
